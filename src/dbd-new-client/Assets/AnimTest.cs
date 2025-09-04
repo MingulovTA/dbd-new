9e7ba986a4a2a3694479bb8800b5cbefdcb9512a
t.mingulov <teimuraz@arpaplus.com>
2025-09-04 16:33:01 +0700

    feat: xbot and y bot and animation added

---
+++ src/dbd-new-client/Assets/AnimTest.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class AnimTest : MonoBehaviour
+{
+    [SerializeField] private Animation _animation;
+    [SerializeField] private List<AnimationClip> _clips;
+    private int _animIndex;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animIndex++;
+            if (_animIndex >= _clips.Count)
+                _animIndex = 0;
+            _animation.CrossFade(_clips[_animIndex].name);
+        }
+    }
+}
