c691040325de28d867667cd663a8892903fef377
t.mingulov <teimuraz@arpaplus.com>
2025-11-12 12:59:16 +0700

    2

--- src/dbd-new-client/Assets/dbd-new/Player/PlayerMovement.cs
+++ src/dbd-new-client/Assets/dbd-new/Player/PlayerMovement.cs
@@ -6,14 +6,10 @@ namespace App.Player
     {
         private const float walkSpeed = 3f;
         private const float jumpHeight = 2f;
-        
         [SerializeField] private PlCrouch _plCrouch;
         [SerializeField] private CharacterController _controller;
-
         private float ySpeed = 0f;
         private float gravity = -9.81f;
-        private Vector3 velocity;
-        private bool isInAir = false;
 
         private void Update()
         {
@@ -22,40 +18,23 @@ namespace App.Player
 
         private void MovePlayer()
         {
-            Vector3 moveDirection = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
-
-            if (moveDirection.magnitude > 1f)
-                moveDirection.Normalize();
-
+            Vector3 moveDirection = transform.right * Input.GetAxis("Horizontal") +
+                                    transform.forward * Input.GetAxis("Vertical");
+            if (moveDirection.magnitude > 1f) moveDirection.Normalize();
             if (_controller.isGrounded)
             {
-                ySpeed = -0.5f; 
-                if (Input.GetButtonDown("Jump"))
-                    ySpeed = Mathf.Sqrt(jumpHeight * -2f * gravity);
-
-                isInAir = false;
+                ySpeed = -0.5f;
+                if (Input.GetButtonDown("Jump")) ySpeed = Mathf.Sqrt(jumpHeight * -2f * gravity);
             }
             else
             {
                 ySpeed += gravity * Time.deltaTime;
-                isInAir = true;
             }
 
-            velocity = moveDirection * walkSpeed;
+            Vector3 velocity = moveDirection * walkSpeed;
             velocity.y = ySpeed;
-
-            if (!_controller.isGrounded && ySpeed > 0 && _plCrouch.IsCeilingDetected())
-                ySpeed = 0;
-
+            if (!_controller.isGrounded && ySpeed > 0 && _plCrouch.IsCeilingDetected()) ySpeed = 0;
             _controller.Move(velocity * Time.deltaTime);
-        }
-
-        public void AddForce(Vector3 force)
-        {
-            if (!isInAir)
-            {
-                velocity += force;
-            }
         }
     }
 }
